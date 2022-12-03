4f80866b2658af776facc7c3ef02dcc0092d7cb3
ltpdbz <118698369+ltpdbz@users.noreply.github.com>
2022-12-03 12:38:39 +0800

    Add files via upload

---
+++ 2022_12_3.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include<windows.h>
+#include<stdlib.h>
+int main(void)
+{
+	char password[20] = {0};
+	int i = 0;
+	for (i = 0; i < 3; i++)
+	{
+		printf("输入密码:");
+		scanf("%s",&password);
+		if (strcmp(password, "123456") == 0)
+		{
+			printf("输入正确\n");
+			break;
+		}
+		else
+		{
+			printf("错误,重新输入\n");
+		}
+	}
+	if (i == 3)
+		printf("输入错误\n");
+	printf("");
+	return 0;
+}
